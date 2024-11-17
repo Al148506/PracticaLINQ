db15608ab6e52d7fba99d1e2c6a5a4b33b8d4871
Al148506 <al148506@gmail.com>
2024-11-17 13:07:05 -0600

    Agregar archivos de proyecto.

---
+++ PracticaLINQ/LinqQueries.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticaLINQ
+{
+    public class LinqQueries
+    {
+        private List<Book> _booksList = new List<Book>();
+        private List<Animal> _animalList = new List<Animal>();
+
+
+        public LinqQueries()
+        {
+            using (StreamReader reader = new StreamReader("books.json"))
+            {
+                string json = reader.ReadToEnd();
+                this._booksList = System.Text.Json.JsonSerializer.Deserialize<List<Book>>
+                    (json, new System.Text.Json.JsonSerializerOptions()
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+     
+        public IEnumerable<Book> AllCollection() { return _booksList; }
+
+        public IEnumerable<Book> CollectionByYear()
+        {
+            //extension method
+            //return _booksList.Where(p=>p.PublishedDate.Year >= 2000 && p.PageCount > 600); }
+            //query expression
+            return from l in _booksList where l.PublishedDate.Year > 2000 select l;
+        }
+        public IEnumerable<Book> CollectionByContent()
+        {
+            //extension method
+            return _booksList.Where(p=>p.Title.Contains("in Action") && p.PageCount > 250); 
+           
+        }
+        // filtra todos los animales que sean de color verde que su nombre inicie con una vocal
+        public IEnumerable<Animal> CollectionByColorAndVocal()
+        {
+            char[] vocales = { 'a', 'e', 'i', 'o', 'u' };
+            Program program = new Program();
+            _animalList = program.AddAnimals().ToList();
+            //extension method
+            var animalesFiltrados = _animalList.Where(p => p.Color.Equals("Gris") && vocales.Contains(char.ToLower(p.Nombre[0])));
+            return animalesFiltrados;
+        }
+
+    }
+}
---
+++ PracticaLINQ/Program.cs
@@ -0,0 +1,59 @@
+﻿using PracticaLINQ;
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        LinqQueries queries = new LinqQueries();
+        Program program = new Program();
+        program.PrintValues(queries.AllCollection());
+        Console.WriteLine("\nLibros del 2000 en adelante\n");
+        program.PrintValues(queries.CollectionByYear());
+        Console.WriteLine("\nLibros con mas de 250 paginas con las palablas in Action\n");
+        program.PrintValues(queries.CollectionByContent());
+        //program.AddAnimals();
+        program.PrintAnimals(queries.CollectionByColorAndVocal());
+
+
+
+
+    }
+    void PrintValues(IEnumerable<Book> booklist)
+    {
+        Console.WriteLine("{0,-60}{1,15}{2,15}\n","Title","N. Pages","Publish Date");
+        foreach(var item in booklist)
+        {
+            Console.WriteLine("{0,-60}{1,15}{2,15}",item.Title,item.PageCount,item.PublishedDate.ToShortDateString());
+        }
+        Console.WriteLine("\nLibros totales: " + booklist.LongCount());
+    }
+
+    public IEnumerable<Animal> AddAnimals()
+    {
+        // Lista de animales
+        List<Animal> animales = new List<Animal>
+        {
+            new Animal() { Nombre = "Hormiga", Color = "Rojo" },
+            new Animal() { Nombre = "Lobo", Color = "Gris" },
+            new Animal() { Nombre = "Elefante", Color = "Gris" },
+            new Animal() { Nombre = "Pantegra", Color = "Negro" },
+            new Animal() { Nombre = "Gato", Color = "Negro" },
+            new Animal() { Nombre = "Iguana", Color = "Verde" },
+            new Animal() { Nombre = "Sapo", Color = "Verde" },
+            new Animal() { Nombre = "Camaleon", Color = "Verde" },
+            new Animal() { Nombre = "Gallina", Color = "Blanco" }
+        };
+        return animales;
+    }
+
+    public void PrintAnimals(IEnumerable<Animal> animales)
+    {
+        Console.WriteLine("Animales de color verde cuyo nombre inicia con una vocal:");
+        foreach (var animal in animales)
+        {
+            Console.WriteLine($"Nombre: {animal.Nombre}, Color: {animal.Color}");
+        }
+    }
+}
